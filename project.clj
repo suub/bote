53055b90b3c7b7b96e75a78005d8a157d53e2ca2
Jan-Paul Bultmann <ticking@me.com>
2014-09-18 12:54:52 +0200

    Upgrades deps.

--- project.clj
+++ project.clj
@@ -5,15 +5,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"conjars" "http://conjars.org/repo"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/tools.reader "0.7.10"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/tools.reader "0.8.8"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/data.priority-map "0.0.5"]
-                 [com.taoensso/timbre "2.6.2"]
+                 [me.raynes/fs "1.4.6"]
+                 [com.taoensso/timbre "3.3.1"]
                  [me.raynes/laser "2.0.0-SNAPSHOT"]
-                 [pandect "0.3.0"]
+                 [pandect "0.3.4"]
                  [suub/error-codes "0.2.0"]
-                 [instaparse "1.3.3"]]
+                 [instaparse "1.3.4"]]
   :resource-paths ["resources"]
-  :plugins [[lein-gorilla "0.3.3"]])
+  :plugins [[lein-gorilla "0.3.4-SNAPSHOT"]])
